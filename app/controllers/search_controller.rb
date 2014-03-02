dd1060f0b996c6b6b06d1a3dfb3077c3018e5815
Renier Morales <renier@morales-rodriguez.net>
2014-03-01 20:20:27 -0400

    Add missing search files

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,18 @@
+class SearchController < ApplicationController
+  def index
+    q = params[:q]
+    terms = q.split(/ /)
+
+    @peoples = []
+    terms.each do |term|
+      @peoples +=
+        Person.fuzzy_search({name: term, lastnames: term, description: term}, false)
+    end
+
+    @churches = []
+    terms.each do |term|
+      @churches +=
+        Church.fuzzy_search({name: term, nickname: term, town: term}, false)
+    end
+  end
+end
