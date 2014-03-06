cffaa77b0d2e8f7589199a30a36142fb7faa6a23
Renier Morales <renier@morales-rodriguez.net>
2014-03-05 20:12:22 -0400

    Remove duplicates from search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,10 +9,14 @@ class SearchController < ApplicationController
         Person.fuzzy_search({name: term, lastnames: term, description: term}, false)
     end
 
+    @peoples.uniq!
+
     @churches = []
     terms.each do |term|
       @churches +=
         Church.fuzzy_search({name: term, nickname: term, town: term}, false)
     end
+
+    @churches.uniq!
   end
 end
