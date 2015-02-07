87f5fca3ea7e32749b9f188c89bdcc33a1b82209
Renier Morales <renierm@us.ibm.com>
2015-02-07 19:58:36 -0400

    Fix refresh of statistics to only happen at the right time

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).on("page:change", function() {
+$(document).on("ready page:change", function(event) {
+    var url;
+    window.clearInterval(window.refreshDashboardTimeout);
+    if (event.originalEvent) {
+        url = event.originalEvent.target.URL;
+        if (url.charAt(url.length - 1) !== "/") { return; }
+    }
     window.refreshDashboardTimeout = setInterval(function() {
         $("#home-link")[0].click();
     }, 60000);
