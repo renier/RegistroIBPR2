215c36ee1ddd06bac4cbac21e9d04d009c034570
Renier Morales <renierm@us.ibm.com>
2016-02-06 14:58:29 -0400

    Update live date

--- config/initializers/registro.rb
+++ config/initializers/registro.rb
@@ -3,7 +3,7 @@ require 'printer'
 module RegistroConfig
   DUE_PER_DELEGATE = 25
   COST_OF_MATERIALS_FOR_VISITOR = 20 # TODO: Is this needed?
-  LIVE_DATE = Time.new(2015, 3, 5)
+  LIVE_DATE = Time.new(2016, 3, 3)
   INKSCAPE_PATH = "inkscape"
   if not defined?(Rails::Console)
     PRINT_AGENT = Printer.new
