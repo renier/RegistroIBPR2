02b9c79c9aee88f768436c3884e0b9fc147d21f5
Renier Morales <renierm@us.ibm.com>
2016-03-04 09:28:31 -0400

    Add role filter to people view

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function filterOnRole() {
+	var role = $('#person-role-filter').val(),
+		url = location.href.replace(/[?&]role=[0-9]?/, '');
+
+	url = url + (url.indexOf('?') !== -1 ? '&' : '?');
+
+	location.href = url + 'role=' + role;
+}
