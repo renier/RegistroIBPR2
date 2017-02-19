f6ece42520858ef4b46905ca4eece65f92877c57
Renier Morales <renier@morales-rodriguez.net>
2017-02-18 21:41:00 -0400

    Fix database settings for registro VM

--- config/initializers/registro.rb
+++ config/initializers/registro.rb
@@ -3,7 +3,7 @@ require 'printer'
 module RegistroConfig
   DUE_PER_DELEGATE = 25
   COST_OF_MATERIALS_FOR_VISITOR = 20 # TODO: Is this needed?
-  LIVE_DATE = Time.new(2016, 3, 3)
+  LIVE_DATE = Time.new(2017, 3, 2)
   #SVG2PDF_CMD = "inkscape -A %{output} %{input}"
   SVG2PDF_CMD = "cairosvg %{input} -o %{output}"
   if not defined?(Rails::Console)
