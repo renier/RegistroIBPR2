8f302a9339e2fefbcdd4111bd732620a1db17d50
Renier Morales <renier@morales-rodriguez.net>
2022-03-01 01:51:24 +0000

    Update the tag logo and masthead

--- lib/tags_helper.rb
+++ lib/tags_helper.rb
@@ -11,8 +11,8 @@ module TagsHelper
     I18n.load_path = Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
     I18n.locale = 'es'
     I18n.default_locale = 'es'
-    logo_right_base = 'ibpr-logo-right-2020.png'
-    logo_left_base = 'ibpr-logo-left-2021.png'
+    logo_right_base = 'ibpr-logo-right-2022.png'
+    logo_left_base = 'ibpr-logo-left-2022.png'
     if browser
       tag = TAG.dup
       logo_right = view_context.image_path logo_right_base
